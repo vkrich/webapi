c921f74438e08a457fd464b626bb416690d0b76e
vkrich <vladislav.krichevsky@mail.ru>
2020-01-29 13:55:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests as rq
+import re
+numbers=[]
+
+with open('dataset_24476_3.txt','r') as f:
+  for i in f:
+    numbers.append(int(i))
+  f.close()
+
+reg=r'boring'
+
+for i in numbers:
+  api_url = 'http://numbersapi.com/' + str(i) + '/' + 'math'
+
+  param = {
+    'type': 'math', 
+    'number': i,
+    'json': 'application/json'
+  }
+
+  res = rq.get(api_url, params = param)
+
+  data=res.json()
+  with open('output.txt','a') as f1:
+    if data['found']:
+      f1.write('Interesting'+'\n')
+    else:
+      f1.write('Boring'+'\n')
+      
+f1.close()    
+  
+  
+  
+  
+
+
